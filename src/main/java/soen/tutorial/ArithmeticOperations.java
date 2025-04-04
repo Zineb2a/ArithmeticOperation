6fab6d771b9ff403fda0fc5cd75951dad28efe67
Zineb Alaoui <99938269+Zineb2a@users.noreply.github.com>
2025-04-04 10:58:56 -0400

    Update ArithmeticOperations.java

--- src/main/java/soen/tutorial/ArithmeticOperations.java
+++ src/main/java/soen/tutorial/ArithmeticOperations.java
@@ -2,6 +2,11 @@ package soen.tutorial;
 
 public class ArithmeticOperations {
 
+	public int divide(int a, int b) {
+    return a / b;
+}
+
+
 	public Integer addOrSub(Integer a, Integer b)
 	{
 		if (a > b) {
